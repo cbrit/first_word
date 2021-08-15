d7f0956aa46340ac1297838d1843c6091fc364f3
Collin Brittain <collinbrittain@gmail.com>
2021-08-14 21:24:24 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let my_string = String::from("This is my string!");
+
+    println!("{}", first_word(&my_string));
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
